09ec84b98ee4fbe9efc037dc988956605803aa4c
Geeyas <geeyasa@gmail.comm>
2025-05-14 20:23:09 +0800

    Register

--- controllers/authController.js
+++ controllers/authController.js
@@ -107,8 +107,6 @@ exports.register = (req, res) => {
                 res.status(201).json({ message: 'User registered successfully', userId });
               });
 
-
-
             }
           );
         });
