4ccab71c14fa646e91b94af2c072c35c5e43e6f9
Geeyas <geeyasa@gmail.comm>
2025-05-15 08:47:46 +0800

    Dynamic Table

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,5 +6,7 @@ const authController = require('../controllers/authController');
 router.post('/login', authController.login);
 router.post('/register', authController.register);
 router.put('/update-password', authController.updatePassword);
+router.get('/tablesInfo', authController.getFullTableInfo);
+router.get('/tables', authController.getAllTables);
 
 module.exports = router;
