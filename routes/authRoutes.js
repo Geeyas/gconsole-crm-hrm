8658e3f4eeb6297801cf48a8bb93ec936326ed9a
Geeyas <geeyasa@gmail.comm>
2025-05-25 00:22:31 +0800

    People Table data Update

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,7 +8,7 @@ router.post('/login', authController.login);
 router.post('/register', authenticate, authController.register);
 
 router.put('/update-password', authenticate, authController.updatePassword);
-router.put('/people/:id', authenticate, authorizeManager, authController.updateUserProfile);
+router.put('/users/:id', authenticate, authorizeManager, authController.updateUserProfile);
 
 
 router.get('/user/:id/usertype', authController.getUsertypeByPersonId);
